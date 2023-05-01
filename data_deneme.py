eb36795f3cf31f9e52a04cf7bbf0e87f16713474
Adem-oss <kmarasing46@gmail.com>
2023-05-01 10:43:49 +0300

    it is my new git code trial

---
+++ data_deneme.py
@@ -0,0 +1,7 @@
+"""bu benim deneme git dosyamdır, burada git komutlarını deniyorum, şimdi adımlarımı özetliyorum
+1-önce temel git kodlarını git versiyonu üzerinde denemek için git windows uygulamasının internet sitesinden
+bularak indiriyorum, burada aslında bu git komutlarının linus işletim sisteminin kodları olduğunu bunu windowsta çalıştırmak için de bu yüklemeyi yapmam gerektiğini anlıyorum.. yükleme tamam. sonra cd, ls, ls -l, ls -a,
+
+mkdir, cd .. ,vim, commit, git log, git status, git init, git log --oneline,git push, git pull gibi komutları öğrenerek bunlar ve daha fazlası ile bir dosyayı bu komutlarla oluşturma bu oluşturulan dosyayı da githup internet sitesine yükleme işlerini yapıyoruz, bunun için kullanıcı adı ve şifre tanımlama işlemini yine git kodalrı ile yapıyoruz öncesinden... şimdi işlemler devam ediyor..:)
+"""
+
